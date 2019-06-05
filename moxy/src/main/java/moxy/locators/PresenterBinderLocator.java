4000682dc255505a6afd783f3a466c23bb07836e
asitnkova <sitnikovsanek1@yandex.ru>
2019-06-05 21:49:11 +0700

    Make PresenterBinderLocator use reflection instead of MoxyReflector

--- moxy/src/main/java/moxy/locators/PresenterBinderLocator.java
+++ moxy/src/main/java/moxy/locators/PresenterBinderLocator.java
@@ -1,7 +1,8 @@
 package moxy.locators;
 
-import moxy.MoxyReflector;
+import moxy.MvpProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PresenterBinderLocator {
@@ -10,6 +11,24 @@ public class PresenterBinderLocator {
     }
 
     public static List<Object> getPresenterBinders(Class<?> delegated) {
-        return MoxyReflector.getPresenterBinders(delegated);
+        //TODO: edit PresenterBinderClassGenerator to generate code, that returns list of super presenter binders
+        ArrayList<Object> binders = new ArrayList<>();
+        Class<?> currentClass = delegated;
+        do {
+            Object presenterBinder = locatePresenterBinder(currentClass);
+            if (presenterBinder != null) {
+                binders.add(presenterBinder);
+            }
+            currentClass = currentClass.getSuperclass();
+        } while (currentClass != null);
+        return binders;
+    }
+
+    private static Object locatePresenterBinder(Class<?> delegated) {
+        try {
+            return Class.forName(delegated.getName() + MvpProcessor.PRESENTER_BINDER_SUFFIX).newInstance();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
