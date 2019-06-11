6b45ae90357d6f49f90043f73fde4dcdddf88e96
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2019-06-11 17:34:47 +0300

    restore code style

--- moxy/src/test/java/moxy/provide_methods_test/resources/LocalProvidedView.java
+++ moxy/src/test/java/moxy/provide_methods_test/resources/LocalProvidedView.java
@@ -6,16 +6,16 @@ import moxy.presenter.ProvidePresenter;
 
 public class LocalProvidedView implements TestView {
 
-    @InjectPresenter
-    public TestPresenter oneLocalPresenter;
+  @InjectPresenter
+  public TestPresenter oneLocalPresenter;
 
-    public TestPresenter oneLocalProvidedPresenter;
+  public TestPresenter oneLocalProvidedPresenter;
 
-    public MvpDelegate<LocalProvidedView> delegate;
+  public MvpDelegate<LocalProvidedView> delegate;
 
-    @ProvidePresenter
-    TestPresenter provideLocalPresenter() {
-        oneLocalProvidedPresenter = new TestPresenter();
-        return oneLocalProvidedPresenter;
-    }
+  @ProvidePresenter
+  TestPresenter provideLocalPresenter() {
+    oneLocalProvidedPresenter = new TestPresenter();
+    return oneLocalProvidedPresenter;
+  }
 }
