9a82a49a06596b3acb46ecaee1def1e2e25e9823
Alexander Blinov <xanderblinov@gmail.com>
2019-06-29 23:02:30 +0300

    correct order

--- script/publish.sh
+++ script/publish.sh
@@ -1,17 +1,17 @@
-./gradlew moxy:bintrayUpload
 ./gradlew moxy:updatePublishVersion
+./gradlew moxy:bintrayUpload
 
-./gradlew moxy-compiler:bintrayUpload
 ./gradlew moxy-compiler:updatePublishVersion
+./gradlew moxy-compiler:bintrayUpload
 
-./gradlew moxy-app-compat:bintrayUpload
 ./gradlew moxy-app-compat:updatePublishVersion
+./gradlew moxy-app-compat:bintrayUpload
 
-./gradlew moxy-androidx:bintrayUpload
 ./gradlew moxy-androidx:updatePublishVersion
+./gradlew moxy-androidx:bintrayUpload
 
-./gradlew moxy-material:bintrayUpload
 ./gradlew moxy-material:updatePublishVersion
+./gradlew moxy-material:bintrayUpload
 
-./gradlew moxy-android:bintrayUpload
 ./gradlew moxy-android:updatePublishVersion
+./gradlew moxy-android:bintrayUpload
