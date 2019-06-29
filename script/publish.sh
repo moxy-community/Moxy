0d0c68a95b2703762ec3e53cd6443a7d5734db7c
Alexander Blinov <xanderblinov@gmail.com>
2019-06-29 20:04:55 +0300

    add publish script

---
+++ script/publish.sh
@@ -0,0 +1,17 @@
+./gradlew moxy:bintrayUpload
+./gradlew moxy:updatePublishVersion
+
+./gradlew moxy-compiler:bintrayUpload
+./gradlew moxy-compiler:updatePublishVersion
+
+./gradlew moxy-app-compat:bintrayUpload
+./gradlew moxy-app-compat:updatePublishVersion
+
+./gradlew moxy-androidx:bintrayUpload
+./gradlew moxy-androidx:updatePublishVersion
+
+./gradlew moxy-material:bintrayUpload
+./gradlew moxy-material:updatePublishVersion
+
+./gradlew moxy-android:bintrayUpload
+./gradlew moxy-android:updatePublishVersion
