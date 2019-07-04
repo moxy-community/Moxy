d2ab7859429fa2ee5a0737b5da5f4088db45b29b
asitnkova <sitnikovsanek1@yandex.ru>
2019-07-04 21:17:19 +0700

    Rename delegate name to moxyPresenter

--- moxy-ktx/src/main/java/moxy/ktx/Delegates.kt
+++ moxy-ktx/src/main/java/moxy/ktx/Delegates.kt
@@ -3,4 +3,4 @@ package moxy.ktx
 import moxy.MvpDelegateHolder
 import moxy.MvpPresenter
 
-fun <T: MvpPresenter<*>> MvpDelegateHolder.presenter(factory: () -> T) = MoxyKtxDelegate(mvpDelegate, factory)
+fun <T : MvpPresenter<*>> MvpDelegateHolder.moxyPresenter(factory: () -> T) = MoxyKtxDelegate(mvpDelegate, factory)
