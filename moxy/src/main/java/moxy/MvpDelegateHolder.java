9785c2629319101828058d6be748562a976e843c
asitnkova <sitnikovsanek1@yandex.ru>
2019-07-08 17:02:47 +0700

    Add documentation to ktx declarations

--- moxy/src/main/java/moxy/MvpDelegateHolder.java
+++ moxy/src/main/java/moxy/MvpDelegateHolder.java
@@ -1,5 +1,12 @@
 package moxy;
 
+/**
+ * Defines class, that contains {@link moxy.MvpDelegate}.
+ * Every call to {@link #getMvpDelegate()} must return same instance.
+ */
 public interface MvpDelegateHolder {
+    /**
+     * @return Instance of MvpDelegate
+     */
     MvpDelegate getMvpDelegate();
 }
