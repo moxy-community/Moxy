c39a7a124046880cf8f79d4eca5adcd8879db4ac
Maksimov Stanislav <bejibx@yandex-team.ru>
2019-07-10 12:01:33 +0300

    Add failing tests for view interface inheritance

--- moxy-compiler/src/test/resources/view/strategies_inheritance/ParentView.java
+++ moxy-compiler/src/test/resources/view/strategies_inheritance/ParentView.java
@@ -2,17 +2,23 @@ package view.strategies_inheritance;
 
 import moxy.MvpView;
 import moxy.viewstate.strategy.StateStrategyType;
+
 import view.strategies_inheritance.strategies.ParentDefaultStrategy;
 import view.strategies_inheritance.strategies.Strategy1;
 
 @StateStrategyType(ParentDefaultStrategy.class)
 public interface ParentView extends MvpView {
-
     void parentMethod1(); // ParentDefaultStrategy
 
     void parentMethod2(); // ParentDefaultStrategy
 
     void parentMethod3(); // ParentDefaultStrategy
+
+    void parentMethodWithArg(String i); // ParentDefaultStrategy
+
+    void parentMethodWithArg2(String i); // ParentDefaultStrategy
+
+    void parentMethodWithArg3(String i); // ParentDefaultStrategy
 
     @StateStrategyType(Strategy1.class)
     void parentMethodWithStrategy(); // Strategy1
