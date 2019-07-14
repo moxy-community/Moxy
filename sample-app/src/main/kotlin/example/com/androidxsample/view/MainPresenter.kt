c0ac67ca5047d0b5434877c1bb200b30439ebd63
Alexander Blinov <xanderblinov@gmail.com>
2019-07-14 16:35:02 +0300

    Добавил ktх в сэмпл

--- sample-app/src/main/kotlin/example/com/androidxsample/view/MainPresenter.kt
+++ sample-app/src/main/kotlin/example/com/androidxsample/view/MainPresenter.kt
@@ -1,24 +1,28 @@
 package example.com.androidxsample.view
 
 import android.util.Log
+import example.com.androidxsample.view.Logger.Companion.MOXY_TAG
 import moxy.InjectViewState
 import moxy.MvpPresenter
 
 @InjectViewState
 class MainPresenter constructor(
-        private val logger: Logger
+    private val logger: Logger
 ) : MvpPresenter<MainView>() {
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
 
-        logger.printErrorLog()
-        Log.e(MainActivity.TAG, "presenter hash code : ${hashCode()}")
+        logger.printLog( "presenter hash code : ${hashCode()}")
         viewState.printLog("TEST")
     }
 
     fun printLog() {
         viewState.printLog("TEST print log ${hashCode()}")
+    }
+
+    fun onOpenKtxButtonClick() {
+        viewState.openKtxActivity()
     }
 
 }
