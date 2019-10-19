7a95a67d8fd20899491318eaac8409e4d0c59da6
asitnkova <sitnikovsanek1@yandex.ru>
2019-10-19 20:07:12 +0700

    Convert ViewInterfaceInfo

--- moxy-compiler/src/main/java/moxy/compiler/viewstate/ViewInterfaceInfo.kt
+++ moxy-compiler/src/main/java/moxy/compiler/viewstate/ViewInterfaceInfo.kt
@@ -4,20 +4,23 @@ import com.squareup.javapoet.ClassName
 import com.squareup.javapoet.ParameterizedTypeName
 import com.squareup.javapoet.TypeName
 import com.squareup.javapoet.TypeVariableName
+import java.util.stream.Collectors
 import javax.lang.model.element.TypeElement
+import javax.lang.model.element.TypeParameterElement
 
 class ViewInterfaceInfo(
     val element: TypeElement,
     val methods: List<ViewMethod>
 ) {
     val name: ClassName = ClassName.get(element)
-    val typeVariables: List<TypeVariableName> = element.typeParameters.map(TypeVariableName::get)
+    val typeVariables: List<TypeVariableName> = element.typeParameters.map { element-> TypeVariableName.get(element) }
 
-    fun getNameWithTypeVariables(): TypeName {
-        return if (typeVariables.isEmpty()) {
-            name
-        } else {
-            ParameterizedTypeName.get(name, *typeVariables.toTypedArray())
+    val nameWithTypeVariables: TypeName
+        get() {
+            return if (typeVariables.isEmpty()) {
+                name
+            } else {
+                ParameterizedTypeName.get(name, *typeVariables.toTypedArray())
+            }
         }
-    }
 }
