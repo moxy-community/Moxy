b384152baee31cd9967cd5e6001dd97c7087bd01
asitnkova <sitnikovsanek1@yandex.ru>
2019-10-19 20:16:57 +0700

    Convert TargetPresenterField

---
+++ moxy-compiler/src/main/java/moxy/compiler/presenterbinder/TargetPresenterField.kt
@@ -0,0 +1,26 @@
+package moxy.compiler.presenterbinder
+
+import com.squareup.javapoet.ParameterizedTypeName
+import com.squareup.javapoet.TypeName
+import moxy.MvpProcessor
+import javax.lang.model.type.TypeMirror
+
+class TargetPresenterField(
+    val clazz: TypeMirror,
+    val name: String,
+    val tag: String?,
+    val presenterId: String?
+) {
+    private val isParametrized = TypeName.get(clazz) is ParameterizedTypeName
+    val typeName: TypeName = if (isParametrized) {
+        (TypeName.get(clazz) as ParameterizedTypeName).rawType
+    } else {
+        TypeName.get(clazz)
+    }
+
+    val generatedClassName: String
+        get() = name.first().toUpperCase() + name.drop(1) + MvpProcessor.PRESENTER_BINDER_INNER_SUFFIX
+
+    var presenterProviderMethodName: String? = null
+    var presenterTagProviderMethodName: String? = null
+}
