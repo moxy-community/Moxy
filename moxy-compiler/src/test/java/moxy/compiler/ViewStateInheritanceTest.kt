18e121a311ababbf4af5b8254d1a7201948ce678
asitnkova <sitnikovsanek1@yandex.ru>
2020-03-18 23:15:04 +0700

    Add new tests covering new cases

--- moxy-compiler/src/test/java/moxy/compiler/ViewStateInheritanceTest.kt
+++ moxy-compiler/src/test/java/moxy/compiler/ViewStateInheritanceTest.kt
@@ -32,14 +32,14 @@ class ViewStateInheritanceTest : CompilerTest() {
     }
 
     @Test
-    fun superInterfaceWithoutStrategiesFailsEvenWithOverride() {
+    fun superInterfaceWithoutStrategiesDoesNotFailWhenOverridden() {
         @Language("JAVA") val view = """
             import moxy.MvpView;
             import moxy.viewstate.strategy.OneExecutionStateStrategy;
             import moxy.viewstate.strategy.StateStrategyType;
             
             interface NoStrategyViewInterface extends MvpView {
-                void someFun(); // <-- fails
+                void someFun();
             }
             
             @StateStrategyType(OneExecutionStateStrategy.class)
@@ -52,10 +52,7 @@ class ViewStateInheritanceTest : CompilerTest() {
 
         val compilation = compileSourcesWithProcessor(view, generateViewStateFor(view.name.substringBefore('.')))
 
-        compilation.assertThatIt()
-            .hadErrorContaining("A View method has no strategy!")
-            .inFile(view)
-            .onLineContaining("<-- fails")
+        compilation.assertSucceeded()
     }
 
     @Test
@@ -83,5 +80,123 @@ class ViewStateInheritanceTest : CompilerTest() {
             .hadErrorContaining("A View method has no strategy!")
             .inFile(view)
             .onLineContaining("<-- fails")
+    }
+
+    @Test
+    fun superInterfaceMethodsClashWarns() {
+        @Language("JAVA") val view = """
+            import moxy.MvpView;
+            import moxy.viewstate.strategy.OneExecutionStateStrategy;
+            import moxy.viewstate.strategy.StateStrategyType;
+            import moxy.viewstate.strategy.alias.AddToEndSingle;
+            import moxy.viewstate.strategy.alias.OneExecution;
+            
+            interface ViewA extends MvpView {
+                @OneExecution
+                void someFun(String bird);
+            }
+            
+            interface ViewB extends MvpView {
+                @AddToEndSingle
+                void someFun(String dog);
+            }
+            
+            public interface ExtendBothViews extends ViewA, ViewB {
+            } 
+        """.toJavaFile()
+
+        val compilation = compileSourcesWithProcessor(view, generateViewStateFor(view.name.substringBefore('.')))
+
+        compilation.assertThatIt()
+            .hadWarningContaining("Strategy clash in superinterfaces of ExtendBothViews. Interface ViewA defines someFun(java.lang.String) with strategy OneExecutionStateStrategy, but ViewB defines this method with strategy AddToEndSingleStrategy. Override this method in ExtendBothViews to choose appropriate strategy")
+            .inFile(view)
+    }
+
+    @Test
+    fun superInterfaceMethodsClashSameStrategySucceeds() {
+        @Language("JAVA") val view = """
+            import moxy.MvpView;
+            import moxy.viewstate.strategy.OneExecutionStateStrategy;
+            import moxy.viewstate.strategy.StateStrategyType;
+            import moxy.viewstate.strategy.alias.OneExecution;
+            
+            interface ViewA extends MvpView {
+                @OneExecution
+                void someFun(int hello);
+            }
+            
+            interface ViewB extends MvpView {
+                @OneExecution
+                void someFun(int moshiMoshi);
+            }
+            
+            public interface ExtendBothViews extends ViewA, ViewB {
+            } 
+        """.toJavaFile()
+
+        val compilation = compileSourcesWithProcessor(view, generateViewStateFor(view.name.substringBefore('.')))
+
+        compilation.assertSucceededWithoutWarnings()
+    }
+
+    @Test
+    fun superInterfaceMethodsClashWithoutStrategiesFailsOnMissingStrategy() {
+        @Language("JAVA") val view = """
+            import moxy.MvpView;
+            import moxy.viewstate.strategy.OneExecutionStateStrategy;
+            import moxy.viewstate.strategy.StateStrategyType;
+            import moxy.viewstate.strategy.alias.OneExecution;
+            
+            interface ViewA extends MvpView {
+                void someFun();
+            }
+            
+            interface ViewB extends MvpView {
+                void someFun();
+            }
+            
+            public interface ExtendBothViews extends ViewA, ViewB {
+            } 
+        """.toJavaFile()
+
+        val compilation = compileSourcesWithProcessor(view, generateViewStateFor(view.name.substringBefore('.')))
+
+        compilation.assertThatIt()
+            .hadErrorContaining("A View method has no strategy!")
+            .inFile(view)
+    }
+
+    @Test
+    fun superInterfaceMethodsClashButOverriddenSucceeds() {
+        @Language("JAVA") val view = """
+            import moxy.MvpView;
+            import moxy.viewstate.strategy.OneExecutionStateStrategy;
+            import moxy.viewstate.strategy.StateStrategyType;
+            import moxy.viewstate.strategy.alias.AddToEnd;
+            import moxy.viewstate.strategy.alias.AddToEndSingle;
+            
+            interface ViewA extends MvpView {
+                @AddToEnd
+                void someFun();
+            }
+            
+            interface ViewB extends MvpView {
+                @AddToEndSingle
+                void someFun();
+            }
+            
+            @StateStrategyType(OneExecutionStateStrategy.class)
+            public interface SomeView extends ViewA, ViewB {
+                
+                @Override
+                void someFun();
+            } 
+        """.toJavaFile()
+
+        val compilation = compileSourcesWithProcessor(view, generateViewStateFor(view.name.substringBefore('.')))
+
+        compilation.assertThatIt()
+            .hadWarningContaining("Strategy clash in superinterfaces of SomeView")
+            .inFile(view)
     }
 }
