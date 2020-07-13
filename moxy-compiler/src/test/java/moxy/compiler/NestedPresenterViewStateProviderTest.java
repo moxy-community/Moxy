b05094344d1c07df56d2d4324d5aa6b8dd94fca0
Sergey Svechnikov <svechnikov66@gmail.com>
2020-07-13 11:34:27 +0500

    Add tests for ViewStateProvider compilation in nested presenters

---
+++ moxy-compiler/src/test/java/moxy/compiler/NestedPresenterViewStateProviderTest.java
@@ -0,0 +1,54 @@
+package moxy.compiler;
+
+import com.google.testing.compile.Compilation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import javax.tools.JavaFileObject;
+
+import moxy.MvpProcessor;
+
+import static com.google.testing.compile.CompilationSubject.assertThat;
+
+@RunWith(Parameterized.class)
+public class NestedPresenterViewStateProviderTest extends CompilerTest {
+
+    @Parameterized.Parameter
+    public TestParams testParams;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static TestParams[] data() {
+        return new TestParams[] {
+            new TestParams(
+                "presenter.PresenterWrapper",
+                "presenter.PresenterWrapper$EmptyViewPresenter" + MvpProcessor.VIEW_STATE_PROVIDER_SUFFIX
+            ),
+        };
+    }
+
+    @Test
+    public void test() throws Exception {
+        JavaFileObject presenter = getSourceFile(testParams.sourceFileName);
+        JavaFileObject exceptedViewStateProvider = getSourceFile(testParams.compiledFileName);
+
+        Compilation presenterCompilation = compileSourcesWithProcessor(presenter);
+        Compilation exceptedViewStateProviderCompilation =
+            compileSources(exceptedViewStateProvider);
+
+        assertThat(presenterCompilation).succeeded();
+        assertExceptedFilesGenerated(presenterCompilation.generatedFiles(),
+            exceptedViewStateProviderCompilation.generatedFiles());
+    }
+
+    private static class TestParams {
+        final String sourceFileName;
+        final String compiledFileName;
+
+        TestParams(String sourceFileName, String compiledFileName) {
+            this.sourceFileName = sourceFileName;
+            this.compiledFileName = compiledFileName;
+        }
+    }
+}
