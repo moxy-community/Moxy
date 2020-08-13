1fc257f549e2892ac3798f2b1f7590af32db16f2
Yevgeny.Khramov <Yevgeny.Khramov@nexign-systems.com>
2020-08-13 09:48:58 +0300

    fixes after code review

--- sample-app/src/test/java/moxy/sample/util/CreatePresenter.kt
+++ sample-app/src/test/java/moxy/sample/util/CreatePresenter.kt
@@ -1,15 +1,17 @@
 package moxy.sample.util
 
 import com.nhaarman.mockitokotlin2.mock
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import moxy.MvpPresenter
 import moxy.MvpView
 
+@ExperimentalCoroutinesApi
 inline fun <P, reified V> createMockPresenterBlocking(
     noinline createPresenter: () -> P,
     noinline createMocks: (suspend () -> Unit)? = null,
     noinline testState: suspend PVContext<P, V>.() -> Unit
-) where P : MvpPresenter<V>, V : MvpView = runBlocking {
+) where P : MvpPresenter<V>, V : MvpView = runBlockingTest {
     createMocks?.invoke()
     val view = mock<V>()
     val presenter = createPresenter()
